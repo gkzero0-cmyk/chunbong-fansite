06d5615f76521843732287bf3736da520e2e4861
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-19 16:36:34 +0900

    fix(pwa): wait for explicit update activation

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,6 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
   );
 });
 
