8abdcfd1a320407847b5fae771357063d2b5fa20
gkzero0-cmyk <gkzero0@gmail.com>
2026-09-19 23:18:30 +0900

    refactor: stop mutating tarot sound preference during preload

--- tarot-sfx-v2-preload.js
+++ tarot-sfx-v2-preload.js
@@ -1,11 +1,4 @@
 (() => {
   const root = typeof window !== 'undefined' ? window : globalThis;
-  const storage = root.localStorage;
-  let enabled = true;
-  try {
-    const stored = storage?.getItem?.('chunbongTarotSound');
-    enabled = stored !== 'off';
-    storage?.setItem?.('chunbongTarotSound', 'off');
-  } catch (_) {}
-  root.__CHUNBONG_TAROT_SFX_PREF__ = { enabled };
+  root.__CHUNBONG_TAROT_ENHANCED_SFX__ = true;
 })();
